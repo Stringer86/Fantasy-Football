c45ae8acfb074c711588214bf062b226dd68fae5
Stringer86 <stringer.stephen@gmail.com>
2016-09-04 12:12:06 -0700

    photo api working

---
+++ js/main.js
@@ -0,0 +1,74 @@
+(function() {
+  'use strict';
+  const $labels = $('.labels');
+  const $container = $('.container');
+
+  function getPhoto1() {
+    let $player1 = $('#player1Name');
+    let $search = $player1.val();
+    let $left = $('#left');
+
+
+    if ($search.length === 0) {
+       Materialize.toast('Please input a player!', 4000);
+       return;
+    }
+
+    let $xhr = $.getJSON(`http://api.cbssports.com/fantasy/players/search?version=3.0&SPORT=football&response_format=json&name=${$search}`);
+
+    $xhr.done(function(data) {
+      if ($xhr.status !== 200) {
+          return;
+      }
+
+      let photoURL = data.body.players[0].photo;
+      let fullName = data.body.players[0].fullname;
+      let $pic1 = $('.pic1');
+      $pic1.attr("src", photoURL);
+      $player1.remove();
+      $labels.remove();
+      $left.append(`<h5>${fullName}</h5>`);
+      $button.remove();
+      $container.append(`<div class="row"><div class="col l6 offset-l5"><a class="#37474f blue-grey darken-3 btn" id="button2">New decision</a></div></div>`);
+
+  });
+
+
+  }
+
+  function getPhoto2() {
+    let $player2 = $('#player2Name');
+    let $search2 = $player2.val();
+    let $right = $('#right');
+
+    if ($search2.length === 0) {
+       Materialize.toast('Please input a player!', 4000);
+       return;
+    }
+
+    let $xhr2 = $.getJSON(`http://api.cbssports.com/fantasy/players/search?version=3.0&SPORT=football&response_format=json&name=${$search2}`);
+
+    $xhr2.done(function(data) {
+      if ($xhr2.status !== 200) {
+          return;
+      }
+
+      let photoURL2 = data.body.players[0].photo;
+      let fullName2 = data.body.players[0].fullname;
+      let $pic2 = $('.pic2');
+      $pic2.attr("src", photoURL2);
+      $player2.remove();
+      $right.append(`<h5>${fullName2}</h5>`);
+
+  });
+}
+  const $button = $('#getIt');
+  $button.click(getPhoto1);
+  $button.click(getPhoto2);
+
+
+
+
+
+
+})();
