33df48633e2ee9e4a33e58e74bb341ed5992bc53
Stringer86 <stringer.stephen@gmail.com>
2016-09-07 15:57:49 -0700

    responsiveness still needs work

--- js/pick.js
+++ js/pick.js
@@ -9,8 +9,10 @@
     let $player1 = $('#player1Name');
     let $search = $player1.val();
 
+
+
     if ($search.length === 0) {
-       Materialize.toast('Need Player 1!', 4000);
+       Materialize.toast('Need Team 1!', 4000);
        return;
     }
 
@@ -20,7 +22,7 @@
       if ($xhr.status !== 200) {
           return;
       }
-console.log("P1:", data);
+
       let photoURL = data.body.players[0].photo;
       let fullName = data.body.players[0].fullname;
       let position = data.body.players[0].position;
@@ -29,7 +31,7 @@ console.log("P1:", data);
       $player1.remove();
       $labels.remove();
       $('.col s5').remove();
-      $left.append(`<h5 class="name1">${fullName}   , ${position}</h5>`);
+      $left.append(`<h5 class="name1">${fullName}</h5>`);
       $button.remove();
 
       // let $button2 = $('#button2');
@@ -44,7 +46,7 @@ console.log("P1:", data);
     let $search2 = $player2.val();
 
     if ($search2.length === 0) {
-       Materialize.toast('Need player 2!', 4000);
+       Materialize.toast('Need Team 2!', 4000);
        return;
     }
 
@@ -54,17 +56,17 @@ console.log("P1:", data);
       if ($xhr2.status !== 200) {
           return;
       }
-console.log("P2:", data);
+
       let photoURL2 = data.body.players[0].photo;
+      console.log(photoURL2);
       let fullName2 = data.body.players[0].fullname;
       let position2 = data.body.players[0].position;
       let $pic2 = $('.pic2');
       $pic2.attr("src", photoURL2);
       $player2.remove();
-      $right.append(`<h5 class="name2">${fullName2}, ${position2}</h5>`);
-      randomPick()
-      otherLinks()
-      createReset()
+      $right.append(`<h5 class="name2">${fullName2}</h5>`);
+      randomPick();// setTimeout to solve race issue
+      createReset();
 
   });
 }
@@ -82,13 +84,13 @@ function randomPick() {
     $player2.addClass("redX");
     $player1.text('\u2714');
     $player2.text('\u2718');
-    $container.append(`<div class="row"><div class="col l12 center"><h5>Stop worrying and start ${$name1}!</div></div>`);
+    $container.append(`<div class="row"><div class="col l12 center"><h5>Stop worrying and pick the ${$name1}!</div></div>`);
   } else {
     $player2.addClass("check");
     $player1.addClass("redX");
     $player2.text('\u2714');
     $player1.text('\u2718');
-    $container.append(`<div class="col l12 center"><h5>Stop worrying and start ${$name2}!</div>`);
+    $container.append(`<div class="col l12 center"><h5>Stop worrying and pick the ${$name2}!</div>`);
   }
 }
 
@@ -96,9 +98,6 @@ function createReset() {
   $container.append(`<div class="row"><div class="s4"></div><div class="col l4 m5 s12 center"><a class="#1565c0 blue darken-3 btn" id="button2" onClick="window.location.reload()">New decision</a></div><div class="s4"></div>`);
 }
 
-function otherLinks() {
-  $container.append(`<div class="row"><div class="col l12"><h5>For 'expert' predictions try <a href="https://www.fantasypros.com/nfl/start/" target="_blank">here</a> or <a href="https://www.fantasysp.com/start-sit-tool/nfl" target="_blank">here</a>`);
-}
 
 
 
---
+++ js/team-autocomplete.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+var data = {
+  "Ravens": null,
+  "Bengals": null,
+  "Steelers": null,
+  "Bears": null,
+  "Lions": null,
+  "Packers": null,
+  "Vikings": null,
+  "Texans": null,
+  "Colts": null,
+  "Jaguars": null,
+  "Titans": null,
+  "Falcons": null,
+  "Panthers": null,
+  "Saints": null,
+  "Buccaneers": null,
+  "Bills": null,
+  "Dolphins": null,
+  "Patriots": null,
+  "Jets": null,
+  "Cowboys": null,
+  "Giants": null,
+  "Eagles": null,
+  "Redskins": null,
+  "Broncos": null,
+  "Chiefs": null,
+  "Raiders": null,
+  "Chargers": null,
+  "Cardinals": null,
+  "49ers": null,
+  "Seahawks": null,
+  "Rams": null,
+  "Browns": null,
+}
+
+
+  $( "input.autocomplete" ).autocomplete({
+    data: data
+
+  });
+});
