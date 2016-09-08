aab27f5a25992c36cb0dd15eef4dbba904e13cb6
Stringer86 <stringer.stephen@gmail.com>
2016-09-07 17:39:00 -0700

    renamed files

--- js/main.js
+++ js/main.js
@@ -1,112 +1,111 @@
 (function() {
-  'use strict';
-  const $labels = $('.labels');
-  const $container = $('.bodyContainer');
-  const $left = $('#left');
-  const $right = $('#right');
+    'use strict';
+    const $labels = $('.labels');
+    const $container = $('.bodyContainer');
+    const $left = $('#left');
+    const $right = $('#right');
+    const $buttonlocation = $('.buttonLocation');
 
-  function getPhoto1() {
-    let $player1 = $('#player1Name');
-    let $search = $player1.val();
+    function getPhoto1() {
+        let $player1 = $('#player1Name');
+        let $search = $player1.val();
 
-    if ($search.length === 0) {
-       Materialize.toast('Need Player 1!', 4000);
-       return;
+        if ($search.length === 0) {
+            Materialize.toast('Need Player 1!', 4000);
+            return;
+        }
+
+        let $xhr = $.getJSON(`https://cors-anywhere.herokuapp.com/http://api.cbssports.com/fantasy/players/search?version=3.0&SPORT=football&response_format=json&name=${$search}`);
+
+        $xhr.done(function(data) {
+            if ($xhr.status !== 200) {
+                return;
+            }
+            console.log("P1:", data);
+            let photoURL = data.body.players[0].photo;
+            let fullName = data.body.players[0].fullname;
+            let position = data.body.players[0].position;
+            let $pic1 = $('.pic1');
+            $pic1.attr("src", photoURL);
+            $player1.remove();
+            $labels.remove();
+            $('.col s5').remove();
+            $left.append(`<h5 class="name1">${fullName}   , ${position}</h5>`);
+            $button.remove();
+
+            // let $button2 = $('#button2');
+            // $button2.click
+        });
+
+
     }
 
-    let $xhr = $.getJSON(`https://cors-anywhere.herokuapp.com/http://api.cbssports.com/fantasy/players/search?version=3.0&SPORT=football&response_format=json&name=${$search}`);
+    function getPhoto2() {
+        let $player2 = $('#player2Name');
+        let $search2 = $player2.val();
 
-    $xhr.done(function(data) {
-      if ($xhr.status !== 200) {
-          return;
-      }
-console.log("P1:", data);
-      let photoURL = data.body.players[0].photo;
-      let fullName = data.body.players[0].fullname;
-      let position = data.body.players[0].position;
-      let $pic1 = $('.pic1');
-      $pic1.attr("src", photoURL);
-      $player1.remove();
-      $labels.remove();
-      $('.col s5').remove();
-      $left.append(`<h5 class="name1">${fullName}   , ${position}</h5>`);
-      $button.remove();
+        if ($search2.length === 0) {
+            Materialize.toast('Need player 2!', 4000);
+            return;
+        }
 
-      // let $button2 = $('#button2');
-      // $button2.click
-  });
+        let $xhr2 = $.getJSON(`https://cors-anywhere.herokuapp.com/http://api.cbssports.com/fantasy/players/search?version=3.0&SPORT=football&response_format=json&name=${$search2}`);
 
+        $xhr2.done(function(data) {
+            if ($xhr2.status !== 200) {
+                return;
+            }
+            console.log("P2:", data);
+            let photoURL2 = data.body.players[0].photo;
+            let fullName2 = data.body.players[0].fullname;
+            let position2 = data.body.players[0].position;
+            let $pic2 = $('.pic2');
+            $pic2.attr("src", photoURL2);
+            $player2.remove();
+            $right.append(`<h5 class="name2">${fullName2}, ${position2}</h5>`);
+            randomPick()
+            otherLinks()
+            createReset()
 
-  }
-
-  function getPhoto2() {
-    let $player2 = $('#player2Name');
-    let $search2 = $player2.val();
-
-    if ($search2.length === 0) {
-       Materialize.toast('Need player 2!', 4000);
-       return;
+        });
     }
 
-    let $xhr2 = $.getJSON(`https://cors-anywhere.herokuapp.com/http://api.cbssports.com/fantasy/players/search?version=3.0&SPORT=football&response_format=json&name=${$search2}`);
 
-    $xhr2.done(function(data) {
-      if ($xhr2.status !== 200) {
-          return;
-      }
-console.log("P2:", data);
-      let photoURL2 = data.body.players[0].photo;
-      let fullName2 = data.body.players[0].fullname;
-      let position2 = data.body.players[0].position;
-      let $pic2 = $('.pic2');
-      $pic2.attr("src", photoURL2);
-      $player2.remove();
-      $right.append(`<h5 class="name2">${fullName2}, ${position2}</h5>`);
-      randomPick()
-      otherLinks()
-      createReset()
+    function randomPick() {
+        let pick = Math.random();
+        let $player1 = $('#player1');
+        let $player2 = $('#player2');
+        let $name1 = $('.name1').text();
+        let $name2 = $('.name2').text();
 
-  });
-}
+        if (pick <= .499) {
+            $player1.addClass("check");
+            $player2.addClass("redX");
+            $player1.text('\u2714');
+            $player2.text('\u2718');
+            $buttonlocation.append(`<div class="col l12 center"><h5>Stop worrying and start ${$name1}!</div>`);
+        } else {
+            $player2.addClass("check");
+            $player1.addClass("redX");
+            $player2.text('\u2714');
+            $player1.text('\u2718');
+            $buttonlocation.append(`<div class="col l12 center"><h5>Stop worrying and start ${$name2}!</div>`);
+        }
+    }
 
+    function createReset() {
+        $buttonlocation.append(`<a class="#1565c0 blue darken-4 btn" id="button2" onClick="window.location.reload()">New decision</a>`);
+    }
 
-function randomPick() {
-  let pick = Math.random();
-  let $player1 = $('#player1');
-  let $player2 = $('#player2');
-  let $name1 = $('.name1').text();
-  let $name2 = $('.name2').text();
-
-  if (pick <= .499) {
-    $player1.addClass("check");
-    $player2.addClass("redX");
-    $player1.text('\u2714');
-    $player2.text('\u2718');
-    $container.append(`<div class="row"><div class="col l12 center"><h5>Stop worrying and start ${$name1}!</div></div>`);
-  } else {
-    $player2.addClass("check");
-    $player1.addClass("redX");
-    $player2.text('\u2714');
-    $player1.text('\u2718');
-    $container.append(`<div class="col l12 center"><h5>Stop worrying and start ${$name2}!</div>`);
-  }
-}
-
-function createReset() {
-  $container.append(`<div class="row"><div class="s4"></div><div class="col l4 m5 s12 center"><a class="#1565c0 blue darken-3 btn" id="button2" onClick="window.location.reload()">New decision</a></div><div class="s4"></div>`);
-}
-
-function otherLinks() {
-  $container.append(`<div class="row"><div class="col l12"><h5>For 'expert' predictions try <a href="https://www.fantasypros.com/nfl/start/" target="_blank">here</a> or <a href="https://www.fantasysp.com/start-sit-tool/nfl" target="_blank">here</a>`);
-}
+    function otherLinks() {
+        $buttonlocation.append(`<div class="row"><div class="col l12"><h5>For 'expert' predictions try <a href="https://www.fantasypros.com/nfl/start/" target="_blank">here</a> or <a href="https://www.fantasysp.com/start-sit-tool/nfl" target="_blank">here</a>`);
+    }
 
 
 
-  const $button = $('#getIt');
-  $button.click(getPhoto1);
-  $button.click(getPhoto2);
-
-
+    const $button = $('#getIt');
+    $button.click(getPhoto1);
+    $button.click(getPhoto2);
 
 
 
--- js/pick.js
+++ js/pick.js
@@ -1,111 +1,110 @@
 (function() {
-  'use strict';
-  const $labels = $('.labels');
-  const $container = $('.bodyContainer');
-  const $left = $('#left');
-  const $right = $('#right');
+    'use strict';
+    const $labels = $('.labels');
+    const $container = $('.bodyContainer');
+    const $left = $('#left');
+    const $right = $('#right');
+    const $buttonlocation = $('.buttonLocation');
 
-  function getPhoto1() {
-    let $player1 = $('#player1Name');
-    let $search = $player1.val();
+    function getPhoto1() {
+        let $player1 = $('#player1Name');
+        let $search = $player1.val();
 
 
 
-    if ($search.length === 0) {
-       Materialize.toast('Need Team 1!', 4000);
-       return;
+        if ($search.length === 0) {
+            Materialize.toast('Need Team 1!', 4000);
+            return;
+        }
+
+        let $xhr = $.getJSON(`https://cors-anywhere.herokuapp.com/http://api.cbssports.com/fantasy/players/search?version=3.0&SPORT=football&response_format=json&name=${$search}`);
+
+        $xhr.done(function(data) {
+            if ($xhr.status !== 200) {
+                return;
+            }
+
+            let photoURL = data.body.players[0].photo;
+            let fullName = data.body.players[0].fullname;
+            let position = data.body.players[0].position;
+            let $pic1 = $('.pic1');
+            $pic1.attr("src", photoURL);
+            $player1.remove();
+            $labels.remove();
+            $('.col s5').remove();
+            $left.append(`<h5 class="name1">${fullName}</h5>`);
+            $button.remove();
+
+            // let $button2 = $('#button2');
+            // $button2.click
+        });
+
+
     }
 
-    let $xhr = $.getJSON(`https://cors-anywhere.herokuapp.com/http://api.cbssports.com/fantasy/players/search?version=3.0&SPORT=football&response_format=json&name=${$search}`);
+    function getPhoto2() {
+        let $player2 = $('#player2Name');
+        let $search2 = $player2.val();
 
-    $xhr.done(function(data) {
-      if ($xhr.status !== 200) {
-          return;
-      }
+        if ($search2.length === 0) {
+            Materialize.toast('Need Team 2!', 4000);
+            return;
+        }
 
-      let photoURL = data.body.players[0].photo;
-      let fullName = data.body.players[0].fullname;
-      let position = data.body.players[0].position;
-      let $pic1 = $('.pic1');
-      $pic1.attr("src", photoURL);
-      $player1.remove();
-      $labels.remove();
-      $('.col s5').remove();
-      $left.append(`<h5 class="name1">${fullName}</h5>`);
-      $button.remove();
+        let $xhr2 = $.getJSON(`https://cors-anywhere.herokuapp.com/http://api.cbssports.com/fantasy/players/search?version=3.0&SPORT=football&response_format=json&name=${$search2}`);
 
-      // let $button2 = $('#button2');
-      // $button2.click
-  });
+        $xhr2.done(function(data) {
+            if ($xhr2.status !== 200) {
+                return;
+            }
 
+            let photoURL2 = data.body.players[0].photo;
+            console.log(photoURL2);
+            let fullName2 = data.body.players[0].fullname;
+            let position2 = data.body.players[0].position;
+            let $pic2 = $('.pic2');
+            $pic2.attr("src", photoURL2);
+            $player2.remove();
+            $right.append(`<h5 class="name2">${fullName2}</h5>`);
+            randomPick(); // setTimeout to solve race issue
+            createReset();
 
-  }
-
-  function getPhoto2() {
-    let $player2 = $('#player2Name');
-    let $search2 = $player2.val();
-
-    if ($search2.length === 0) {
-       Materialize.toast('Need Team 2!', 4000);
-       return;
+        });
     }
 
-    let $xhr2 = $.getJSON(`https://cors-anywhere.herokuapp.com/http://api.cbssports.com/fantasy/players/search?version=3.0&SPORT=football&response_format=json&name=${$search2}`);
 
-    $xhr2.done(function(data) {
-      if ($xhr2.status !== 200) {
-          return;
-      }
+    function randomPick() {
+        let pick = Math.random();
+        let $player1 = $('#player1');
+        let $player2 = $('#player2');
+        let $name1 = $('.name1').text();
+        let $name2 = $('.name2').text();
 
-      let photoURL2 = data.body.players[0].photo;
-      console.log(photoURL2);
-      let fullName2 = data.body.players[0].fullname;
-      let position2 = data.body.players[0].position;
-      let $pic2 = $('.pic2');
-      $pic2.attr("src", photoURL2);
-      $player2.remove();
-      $right.append(`<h5 class="name2">${fullName2}</h5>`);
-      randomPick();// setTimeout to solve race issue
-      createReset();
+        if (pick <= .499) {
+            $player1.addClass("check");
+            $player2.addClass("redX");
+            $player1.text('\u2714');
+            $player2.text('\u2718');
+            $buttonlocation.append(`<div class="col l12 center"><h5>Stop worrying and pick the ${$name1}!</div>`);
+        } else {
+            $player2.addClass("check");
+            $player1.addClass("redX");
+            $player2.text('\u2714');
+            $player1.text('\u2718');
+            $buttonlocation.append(`<div class="col l12 center"><h5>Stop worrying and pick the ${$name2}!</div>`);
+        }
+    }
 
-  });
-}
-
-
-function randomPick() {
-  let pick = Math.random();
-  let $player1 = $('#player1');
-  let $player2 = $('#player2');
-  let $name1 = $('.name1').text();
-  let $name2 = $('.name2').text();
-
-  if (pick <= .499) {
-    $player1.addClass("check");
-    $player2.addClass("redX");
-    $player1.text('\u2714');
-    $player2.text('\u2718');
-    $container.append(`<div class="row"><div class="col l12 center"><h5>Stop worrying and pick the ${$name1}!</div></div>`);
-  } else {
-    $player2.addClass("check");
-    $player1.addClass("redX");
-    $player2.text('\u2714');
-    $player1.text('\u2718');
-    $container.append(`<div class="col l12 center"><h5>Stop worrying and pick the ${$name2}!</div>`);
-  }
-}
-
-function createReset() {
-  $container.append(`<div class="row"><div class="s4"></div><div class="col l4 m5 s12 center"><a class="#1565c0 blue darken-3 btn" id="button2" onClick="window.location.reload()">New decision</a></div><div class="s4"></div>`);
-}
+    function createReset() {
+        $buttonlocation.append(`<a class="#1565c0 blue darken-4 btn" id="button2" onClick="window.location.reload()">New decision</a>`);
+    }
 
 
 
 
-  const $button = $('#getIt');
-  $button.click(getPhoto1);
-  $button.click(getPhoto2);
-
-
+    const $button = $('#getIt');
+    $button.click(getPhoto1);
+    $button.click(getPhoto2);
 
 
 
